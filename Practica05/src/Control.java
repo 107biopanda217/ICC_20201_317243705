5fe7d5da322d71e8906edf8b8a8a271d42da1b85
107biopanda217 <107biopanda217>
2019-10-11 20:23:49 -0500

    Práctica 05 intento

--- Practica05/src/Control.java
+++ Practica05/src/Control.java
@@ -1,20 +1,49 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Práctica 05. Variables de control, arreglos y algoritmos.
+ * Objetivo: mejorar el manejo de las variables de control.
+ * Desarrollo: resolver problemas simples completando el método. 
+ *  @autor Andrea
+ **/
+
 public class Control{
+
+    /**
+     * Problema 1. Identifica si una palabra es palíndromo, 
+     * por ejemplo “reconocer” es un palíndromo y  agua no lo es.
+     * @param palabra -- Palabra que se recibe del usuario.
+     * @return true- La palabra es palíndromo.
+     * @return false -- La palabra no es palíndromo.
+     */
 
     public static boolean problema1(String palabra){
         char[] arreglo1 = palabra.toCharArray();
-        int longitud = arreglo1.length; 
-        for(int n = 0; n < arreglo1.length; n++){
-            if(arreglo1[n] != arreglo1[arreglo1.length]){
+        int longitud = arreglo1.length-1; 
+        for(int n = 0; n < longitud; n++){
+            if(arreglo1[n] != arreglo1[longitud]){
                 return false;    
             }
-            longitud--; 
+            for(int m = longitud; longitud >= n; longitud--){
+                return true;
+            }
         }
         return true;
     }
     
+    /**
+     * Problema 2. Dado un arreglo A y un número K, 
+     * determinar si hay 2 números en el arreglo que sumen K.
+     * Por ejemplo [10, 15, 3, 7] y K = 17, regresa true porque 10 + 7 es 17.
+     * @param arreglo -- Arreglo de números enteros que proporciona el usuario.
+     * @param k -- Número entero a rectificar.
+     * @return true -- El número entero a rectificar es suma de dos de los 
+     *                 números enteros pertenecientes al arreglo.
+     * @return false -- El npumero entero a rectificar no es suma de dos de los
+     *                  números enteros pertenecientes al arreglo.
+     */
+
     public static boolean problema2(int[] arreglo, int k){
         for(int a = 0; a < arreglo.length; a++){
             for(int fin = arreglo.length-1; fin >= a; fin--){
@@ -25,19 +54,43 @@ public class Control{
         }
         return false;
     }
-    
-    
-     public static void problema3(int[] arreglo){
-     /*   for(int x = 0; x < arreglo.length; x++){
-            int[] arregloUno = new int[arreglo.length];
-            return Arrays.sort(arregloUno);
-        }
+   
+    /**
+     * Problema 3. Dado un arreglo A, retornar el mismo arreglo pero ordenado.
+     * @param arreglo -- Arreglo de enteros dado por el usuario.
      */
-     }
-    
+
+    public static void problema3(int[] arreglo){
+        int [] arregloUno = new int[arreglo.length];
+        Arrays.sort(arregloUno);
+        for(int i = 0; i < arregloUno.length; i++){
+            System.out.println(arregloUno[i]);
+        } 
+    }
+
+    /**
+     * Problema 4. Dado un arreglo ordenado A, busca un número K.
+     * @param arreglo -- Arreglo ordenado que proporciona el usuario.
+     * @param buscando -- Número entero que se busca dentro del arreglo ordenado.
+     * @return true -- El número entero sí está en el arreglo ordenado.
+     * @return false -- EL número entero no está en el arreglo ordenado.
+     */
+
     public static boolean problema4(int[] arreglo, int buscando){
+        int k = buscando;
+        for(int i = 0; i < arreglo.length; i++){
+            if(arreglo[i] != k) return false;
+        }
         return true;
     }
+
+    /**
+     * Problema 5. Saber si un número K es primo.
+     * Por ejemplo 17 regresa true, porque es primo. 
+     * @param n -- Número entero dado por el usuario para verificar si es primo.
+     * @return true -- El número entero es primo.
+     * @return false -- El número entero no es primo.
+     */
     
     public static boolean problema5(int n){
         if (n == 1) return false;
@@ -47,6 +100,15 @@ public class Control{
         }
         return true;
     }
+
+    /** 
+     * Problema 6. Regresa todos los números primos menores a N. 
+     * Por ejemplo si N es 18 los números primos menores son 2, 3, 5, 7, 11, 13, 17.
+     * @param n -- Número entero dado por el usuario, del cual se van a
+     *             regresar los números primos que le anteceden.
+     * @return primos -- Arreglo compuesto por los números primos que anteceden 
+     *                   al número entero dado.
+    **/
     
     public static int[] problema6(int n){
         ArrayList<Integer> primos = new ArrayList<>();
@@ -55,8 +117,15 @@ public class Control{
                 primos.add(i);
             }
         }
-         return convierte(primos);
+        return convierte(primos);
     }
+
+    /**
+     * Método complemento problema6.
+     * Convierte una lista en un arreglo.
+     * @param lista -- Lista dada.
+     * @return arreglo -- Regresa el arreglo que contiene los elementos de la lista anterior.
+     */
 
     public static int[] convierte(ArrayList<Integer> lista){
         int[] arreglo = new int[lista.size()];
@@ -66,8 +135,11 @@ public class Control{
         return arreglo;
     }
     
-
-
+    /**
+     * Pruebas que ayudan a comprobar si la solución a los problemas anteriores
+     * es correcta.
+     * @param args -- Cadena de arreglos.
+     */
     public static void main(String[] args) {
         String resultado = "";
         int[] desordenado = {9,8,7,6,5,4,3,2,1};
@@ -82,5 +154,4 @@ public class Control{
         if(Arrays.equals(problema6(10), primos10) == true)resultado+="P6 bien\n";else resultado+="P6 mal\n";
         System.out.println(resultado);   
     }
-
 }
